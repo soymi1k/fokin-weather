b83170067bf459cbc1d86b100aade296428b4093
soymi1k <soymilkneigh@gmail.com>
2021-01-05 17:11:11 +0900

    Layout with Flexbox

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.purpleView} />
+      <View style={styles.greenView} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  purpleView: {
+    flex: .35,
+    backgroundColor: "purple"
+  },
+  greenView: {
+    flex: 7,
+    backgroundColor: "green"
+  }
 });
