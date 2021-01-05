d3484df3252060539c7156d6100407342ab46f4b
soymi1k <soymilkneigh@gmail.com>
2021-01-05 17:25:22 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.purpleView} />
-      <View style={styles.greenView} />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  purpleView: {
-    flex: .35,
-    backgroundColor: "purple"
-  },
-  greenView: {
-    flex: 7,
-    backgroundColor: "green"
-  }
-});
