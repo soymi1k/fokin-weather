25ea7e49814cd540355e276ec54113ac4a94238e
soymi1k <soymilkneigh@gmail.com>
2021-01-19 18:21:36 +0900

    #1.4 analy

--- App.js
+++ App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
+import axios from 'axios';
 
+const API_KEY = "d98bfe42caa2a0e87721c5e20e10ae4a"
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async(latitude, longitude) =>{
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    console.log(data);
+  };
   getLocation = async() => {
     try {
       await Location.requestPermissionsAsync(); //위치값 유저 허가 대기 await 은 기다리라는 뜻 (async()) = "wait for it" getCurrentPositionAsync()만 실행했을 때 먼저 permission 받으라고 경고가 뜸
-      const { coords } = await Location.getCurrentPositionAsync(); //현재 위치값의 정보를 coords로 상수에 저장 (es6를 이용해getCurrentPositionAsync()의 coords 오브젝트 값을 바로 저장한다)
-      console.log(coords.latitude, coords.longitude); // Send to API and get weather
+      const {
+        coords : {latitude, longitude} 
+      } = await Location.getCurrentPositionAsync(); //현재 위치값의 정보를 coords로 상수에 저장 (es6를 이용해getCurrentPositionAsync()의 coords 오브젝트 값을 바로 저장한다)
+      this.getWeather(latitude, longitude)
+
       this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
