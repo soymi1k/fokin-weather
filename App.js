d50295bee8a12a62149c7bc99712f96af31d8fbe
soymi1k <soymilkneigh@gmail.com>
2021-01-19 11:06:10 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -5,20 +5,24 @@ import * as Location from 'expo-location';
 
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
   getLocation = async() => {
     try {
-      await Location.requestPermissionsAsync();
-      const { coords } = await Location.getCurrentPositionAsync();
-      console.log(coords.latitude, coords.longitude);
-      
+      await Location.requestPermissionsAsync(); //위치값 유저 허가 대기 await 은 기다리라는 뜻 (async()) = "wait for it" getCurrentPositionAsync()만 실행했을 때 먼저 permission 받으라고 경고가 뜸
+      const { coords } = await Location.getCurrentPositionAsync(); //현재 위치값의 정보를 coords로 상수에 저장 (es6를 이용해getCurrentPositionAsync()의 coords 오브젝트 값을 바로 저장한다)
+      console.log(coords.latitude, coords.longitude); // Send to API and get weather
+      this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   }
   componentDidMount(){
-    this.getLocation();
+    this.getLocation(); // componentDidMount로 페이지 로딩후 getLocation 함수를 호출
   }
   render(){
-    return<Loading />;
+    const {isLoading} = this.state;
+    return isLoading ? <Loading />: null;
   } 
 }
