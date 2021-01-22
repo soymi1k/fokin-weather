9e2b9590a7792036da092847e290d0e7ec82de3e
soymi1k <soymilkneigh@gmail.com>
2021-01-22 09:26:03 +0900

    #2.1 Getting the Condition Names

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
-import Weather from "./Weather"; //Weather 파일을 Weather로 호출
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
-const API_KEY = "d98bfe42caa2a0e87721c5e20e10ae4a";
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
-
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
@@ -23,19 +37,15 @@ export default class extends React.Component {
       Alert.alert("Can't find you.", "So sad");
     }
   };
-
-  getWeather = async (potato, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${potato}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    ); //openWeather Doc에 unit of measurement를 보면 &units=metric를 url에 추가해야 섭씨로 나온다
-    this.setState({ isLoading: false, temp: data.main.temp }); //data Object의 temp값을 temp에 넣고, isLoading값을 false로 (getLocation async함수에서 옮겨옴)
-  };
-
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp } = this.state; // state에 temp 선언
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />; // Weather파일에 temp값을 Math.round로 정수로 보내준다
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
