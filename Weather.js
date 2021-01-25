c13c2e63d62344757e3f39de4f079a39980e6192
soymi1k <soymilkneigh@gmail.com>
2021-01-25 09:52:03 +0900

    End weather app

--- Weather.js
+++ Weather.js
@@ -8,22 +8,32 @@ const weatherOptions = {
   Mist: {
     iconName: "weather-cloudy",
     gradient: ["#8CA6DB", "#B993D6"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on.",
   },
   Thunderstorm: {
     iconName: "weather-light",
     gradient: ["#373B44", "#4286f4"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house",
   },
   Drizzle: {
     iconName: "weather-rainy",
     gradient: ["#2ebf91", "#8360c3"],
+    title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈",
   },
   Rain: {
     iconName: "weather-pouring",
     gradient: ["#19547b", "#ffd89b"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#403B4A", "#E7E9BB"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman? Fuck no.",
   },
   Atmosphere: {
     iconName: "weather-sunny-alert",
@@ -32,25 +42,32 @@ const weatherOptions = {
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#77A1D3", "#79CBCA", "#E684AE"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#4e4376", "#2b5876"],
+    title: "Cloud",
+    subtitle: "This is Blumeing time",
   },
   Haze: {
     iconName: "weather-hazy",
     gradient: ["#292E49", "#536976", "#BBD2C5"],
+    title: "Haze",
+    subtitle: "Just don't go outside.",
   },
   Dust: {
     iconName: "weather-fog",
     gradient: ["#753a88", "#cc2b5e"],
+    title: "Dusty",
+    subtitle: "Thanks a lot China 🖕🏻",
   },
 };
 
 export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
-      // Background Linear Gradient
       colors={weatherOptions[condition].gradient}
       style={styles.container}
     >
@@ -63,7 +80,12 @@ export default function Weather({ temp, condition }) {
         />
         <Text style={styles.temp}>{temp}ºc</Text>
       </View>
-      <View style={styles.halfContainer} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
     </LinearGradient>
   );
 }
@@ -87,8 +109,8 @@ Weather.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
   },
   temp: {
     fontSize: 42,
@@ -98,5 +120,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+    textAlign: "left",
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 24,
+    textAlign: "left",
+  },
+  textContainer: {
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1,
   },
 });
